a804ea29164e876f8809f772fc38a04e555c9f41
agustincoda <codaagustin@gmail.com>
2024-05-02 23:47:26 -0300

    cambiado la logica de aprobarAnio y configurado un poco el html

--- js/main.js
+++ js/main.js
@@ -6,49 +6,71 @@ class alumno {
         this.notas.geografia=notasGeografia;
         this.notas.historia=notasHistoria;
     }
+
+    promedioNotas(materia){
+        let notasArray = 0;
+        this.notas[materia].forEach(nota => {
+            notasArray += nota;
+        });
+        const promedio= notasArray/this.notas[materia].length;
+        return promedio;
+    }
     aproboMateria(materia) {
-        const ultimaNotaCargada = this.notas.materia[ (this.notas.materia.length) -1 ];
-        if (ultimaNotaCargada>6){
+        const promedio = this.promedioNotas(materia);
+        if (promedio>=6){
             return true;
         }else{
             return false;
         }
     }
-    aproboanio(){
-        if(this.aproboMateria(this.notas.matematicas)&&this.aproboMateria(this.notas.geografia)&&this.aproboMateria(this.notas.historia)){
+    aproboAnio(){
+        if(this.aproboMateria("matematicas")&&this.aproboMateria("geografia")&&this.aproboMateria("historia")){
             alert(this.nombre + " aprobó el año!");
-            console.log (this.nombre + " aprobó el año!");
         }else{
-            alert(this.nombre + " va a tener que repetir, lo lamento");
-            console.log(this.nombre + " va a tener que repetir, lo lamento");
+            alert(this.nombre + " va a tener que repetir, lo lamento.");
         }
     }
+    notasPromocion(materia){
+        const notasAltas=this.notas[materia].filter((notas)=>notas>=8);
+        if(notasAltas.length==0){
+            console.log("Ninguna nota alta?\nHay que esforzarse para la proxima.");
+        }else{
+            console.log("las notas para promocion de " + materia + " fueron " + notasAltas);
+        }
+    }
+
 }
 
 
-let cantidadAlumnos=parseInt(prompt("ingrese el numero de alumnos"));;
-let notaMatematicas=[];
-let notaGeografia=[];
-let notaHistoria=[];
+let cantidadAlumnos=parseInt(prompt("Ingrese el numero de alumnos"));;
+
 for(let i=0;i<cantidadAlumnos;i++){
-    let nombreAlumno = prompt("Ingresa el nombre del alumno");
-    
-    do{
-        let cargar=parseInt(prompt("ingresa las notas de matematicas"));
+    let notaMatematicas=[];
+    let notaGeografia=[];
+    let notaHistoria=[];
+    let nombreAlumno = prompt("Ingresa el nombre del alumno.");
+    let examenesMate=parseInt(prompt("Cuantos examenes rindio de Matematicas?"));
+    let examenesGeo=parseInt(prompt("Cuantos examenes rindio de Geografia?"));
+    let examenesHisto=parseInt(prompt("Cuantos examenes rindio de Historia?"));
+    do{ 
+        let cargar=parseInt(prompt("ingresa las notas de matematicas."));
         notaMatematicas.push(cargar);
-    }while(notaMatematicas.length!=3);
+        console.log(notaMatematicas);
+    }while(notaMatematicas.length<examenesMate);
     do{
-        let cargar=parseInt(prompt("ingresa las notas de geografia"));
+        let cargar=parseInt(prompt("ingresa las notas de geografia."));
         notaGeografia.push(cargar);
-    }while(notaGeografia.length!=3);
-    do{
-        let cargar=parseInt(prompt("ingresa las notas de historia"));
+        console.log(notaGeografia);
+    }while(notaGeografia.length<examenesGeo);
+    do{  
+        let cargar=parseInt(prompt("ingresa las notas de historia."));
         notaHistoria.push(cargar);
-    }while(notaHistoria.length!=3);
+        console.log(notaHistoria);
+    }while(notaHistoria.length<examenesHisto);
 
     
 const nuevoAlumno= new alumno (nombreAlumno,notaMatematicas,notaGeografia,notaHistoria);
-nuevoAlumno.aproboanio();
-
+nuevoAlumno.aproboAnio();
+nuevoAlumno.notasPromocion("matematicas");
 
 }
